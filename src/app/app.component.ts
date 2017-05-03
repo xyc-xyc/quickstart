52cfa8c1de4e0db35cfe357d614aa6d1a7946f99
stone <stone@stones-MBP.home>
2017-05-02 22:47:51 -0400

    ++ primeng treetable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,102 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<p-treeTable [value]="files">
+    <p-header>Basic</p-header>
+    <p-column field="name" header="Name"></p-column>
+    <p-column field="size" header="Size"></p-column>
+    <p-column field="type" header="Type"></p-column>
+</p-treeTable>
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+    count  = 0; 
+    clk = () => {
+      this.count ++;
+    }
+
+    files = 
+    [  
+        {  
+            "data":{  
+                "name":"Documents",
+                "size":"75kb",
+                "type":"Folder"
+            },
+            "children":[
+                {  
+                    "data":{  
+                        "name":"Work",
+                        "size":"55kb",
+                        "type":"Folder"
+                    },
+                    "children":[  
+                        {  
+                            "data":{  
+                                "name":"Expenses.doc",
+                                "size":"30kb",
+                                "type":"Document"
+                            }
+                        },
+                        {  
+                            "data":{  
+                                "name":"Resume.doc",
+                                "size":"25kb",
+                                "type":"Resume"
+                            }
+                        }
+                    ]
+                },
+                {  
+                    "data":{  
+                        "name":"Home",
+                        "size":"20kb",
+                        "type":"Folder"
+                    },
+                    "children":[  
+                        {  
+                            "data":{  
+                                "name":"Invoices",
+                                "size":"20kb",
+                                "type":"Text"
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {  
+            "data":{  
+                "name":"Pictures",
+                "size":"150kb",
+                "type":"Folder"
+            },
+            "children":[  
+                {  
+                    "data":{  
+                        "name":"barcelona.jpg",
+                        "size":"90kb",
+                        "type":"Picture"
+                    }
+                },
+                {  
+                    "data":{  
+                        "name":"primeui.png",
+                        "size":"30kb",
+                        "type":"Picture"
+                    }
+                },
+                {  
+                    "data":{  
+                        "name":"optimus.jpg",
+                        "size":"30kb",
+                        "type":"Picture"
+                    }
+                }
+            ]
+        }
+    ]
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {ButtonModule,TreeTableModule,TreeNode,SharedModule} from 'primeng/primeng';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, ButtonModule , TreeTableModule, SharedModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
